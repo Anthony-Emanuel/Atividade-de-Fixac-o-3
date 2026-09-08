296e061e48ebb085c6f89b90add4c6b67d5bc58c
Anthony-Emanuel <anthony.211106@gmail.com>
2026-09-08 10:29:06 -0400

    Update questão01.c

--- questão01.c
+++ questão01.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    float media;
+    int dia;
 
-    printf("Digite a media final: ");
-    scanf("%f", &media);
+    printf("Digite um numero de 1 a 7: ");
+    scanf("%d", &dia);
 
-    if (media >= 7.0) {
-        printf("Aprovado\n");
-    } else {
-        printf("Reprovado\n");
+    switch (dia) {
+        case 1:
+            printf("Domingo\n");
+            break;
+        case 2:
+            printf("Segunda-feira\n");
+            break;
+        case 3:
+            printf("Terca-feira\n");
+            break;
+        case 4:
+            printf("Quarta-feira\n");
+            break;
+        case 5:
+            printf("Quinta-feira\n");
+            break;
+        case 6:
+            printf("Sexta-feira\n");
+            break;
+        case 7:
+            printf("Sabado\n");
+            break;
+        default:
+            printf("Dia invalido\n");
     }
 
     return 0;
